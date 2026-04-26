becd2ee8366dc2afa37914bc02ba259859b56002
Maidane <aboubakeradanmaidane@gmail.com>
2026-04-26 18:11:48 +0300

    Activation debug et variables environnement

--- config.php
+++ config.php
@@ -1,15 +1,22 @@
 <?php
-$host = "localhost";
+// Configuration pour la base de données en ligne (Railway)
+$host = "shortline.proxy.rlwy.net";
 $user = "root";
-$pass = "";
-$dbname = "marketplace_db";
+$pass = "rFmaCdILzPKAUAJTANFUIIkioRRJlKVO";
+$dbname = "railway";
+$port = 18188;
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+// Connexion MySQLi
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
+// Vérification de la connexion
 if ($conn->connect_error) {
-    die("La connexion a échoué : " . $conn->connect_error);
+    die("La connexion au serveur Railway a échoué : " . $conn->connect_error);
 }
 
-// Pour supporter les accents et les emojis
+// Support des caractères spéciaux (accents, etc.)
 $conn->set_charset("utf8mb4");
+
+// Echo pour test (à supprimer une fois que ça marche)
+// echo "Félicitations Aboubaker ! Connexion au Cloud réussie.";
 ?>
